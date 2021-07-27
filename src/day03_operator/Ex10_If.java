1d6ff5252a344ee47e798b9c0e2ca0aaccabf3c5
Choi.SY <sollly0829@gmail.com>
2021-07-27 13:10:37 +0900

    Ex01_If if문 예시

---
+++ src/day03_operator/Ex10_If.java
@@ -0,0 +1,20 @@
+package day03_operator;
+
+import java.util.Scanner;
+
+public class Ex10_If {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int num;
+		System.out.print("¼ö ÀÔ·Â : ");
+		num=sc.nextInt();
+		
+		if(num%2==0) {
+			System.out.println("Â¦¼ö");
+		} if (num%2!=0) {
+			System.out.println("È¦¼ö");
+		}
+	}
+}
